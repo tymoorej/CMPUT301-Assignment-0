375dae2d450e50507ca2937dcd2e9ce2a08ae99d
Tymoore Jamal <tymoore@ualberta.ca>
2019-01-08 17:30:50 -0700

    Finished project by adding button which transfered message from one screen to another

--- app/src/main/java/com/example/tymoore/cmput301assignment0/MainActivity.java
+++ app/src/main/java/com/example/tymoore/cmput301assignment0/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.tymoore.cmput301assignment0;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.tymoore.cmput301assignment0.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.inputText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
 }
